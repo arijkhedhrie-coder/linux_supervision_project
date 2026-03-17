5ba3a1334dcb20351fd9853a0d4421d1ea60e084
arijkhedhrie-coder <arij.khedhrie@gmail.com>
2026-03-17 20:39:55 +0100

    Add AWS configuration variables to config.py

--- config/config.py
+++ config/config.py
@@ -8,6 +8,14 @@ AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
+
+
+
+
+BUCKET_NAME    = os.getenv("S3_BUCKET_NAME", "pfe-linux-logs-supervision")
+AWS_REGION     = os.getenv("AWS_REGION", "us-east-1")
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 CHEMIN_RAW  = r"C:\Users\LENOVO\pfe-linux-supervision\data\raw"
 SERVEUR_NOM = "server1"
 ANNEE       = "2026"
